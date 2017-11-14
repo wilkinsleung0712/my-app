0cc8dea14508ab8bc19adaea72ec78d729f070ec
Wilkins Liang <wilkins.liang@fluentretail.com>
2017-11-14 15:55:48 +1100

    adding history to show time travel

--- src/index.js
+++ src/index.js
@@ -63,7 +63,9 @@ class Game extends React.Component {
                     squares: Array(9).fill(null),
                     xIsNext: true
                 }
-            ]
+            ],
+            xIsNext: true,
+            stepNumber: 0
         }
     }
 
@@ -71,7 +73,9 @@ class Game extends React.Component {
         const history = this.state.history;
         const current = history[history.length - 1];
 
-        const squares = current.squares.slice();
+        const squares = current
+            .squares
+            .slice();
 
         if (calculateWinner(squares) || squares[i]) {
             return;
@@ -80,18 +84,41 @@ class Game extends React.Component {
             ? 'X'
             : 'O';
         this.setState({
-            history: this.state.history.concat([{
-                squares: squares,
-               
-            }]),
+            history: this
+                .state
+                .history
+                .concat([
+                    {
+                        squares: squares
+                    }
+                ]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext
+        });
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0
         });
     }
 
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
+
+        const moves = history.map((step, move) => {
+            const desc = move
+                ? 'Go to move #' + move
+                : 'Go to game start';
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
 
         let status;
         if (winner) {
@@ -105,13 +132,11 @@ class Game extends React.Component {
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board squares={current.squares} onClick={
-                        (i) => this.handleClick(i)
-                        }/>
+                    <Board squares={current.squares} onClick={(i) => this.handleClick(i)}/>
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                    <ol>{/* TODO */}</ol>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
